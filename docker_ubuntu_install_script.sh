5916e40c6bec7872b107f0fa08a34834c046364d
bgenchel <benjiegenchel@gmail.com>
2018-03-05 23:36:38 -0500

    bashrc back in the game

--- docker_ubuntu_install_script.sh
+++ docker_ubuntu_install_script.sh
@@ -64,7 +64,7 @@ python $DOTFILES/.vim/plugged/youcompleteme/install.py --clang-completer
 # vim +PlugInstall +qall
 
 echo "clearing out old files ..."
-# if [ -a /.bashrc ]; then rm -r /.bashrc; fi
+if $(test -e /.bashrc ); then rm -r /.bashrc; fi
 if $(test -e /.vimrc ); then rm -r $HOME/.vimrc; fi
 if $(test -e /.vim ); then yes | rm -r $HOME/.vim; fi
 if $(test -e /.tmux.conf); then rm -r $HOME/.tmux.conf; fi
@@ -72,7 +72,7 @@ if $(test -e /.zshrc); then rm -r $HOME/.zshrc; fi
 if $(test -e /.oh-my-zsh); then yes | rm -r $HOME/.oh-my-zsh; fi
 
 echo "placing new files ..."
-# ln -s $DOTFILES/.bashrc /.bashrc
+cp $DOTFILES/.bashrc $HOME/.bashrc
 cp $DOTFILES/.vimrc $HOME/
 cp -r $DOTFILES/.vim $HOME/
 cp $DOTFILES/.tmux.conf $HOME/
