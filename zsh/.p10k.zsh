ffb4c5cd38794be8a7d38ac999e4c113e26370f9
Benjamin Genchel <benjieg@spotify.com>
2022-09-21 16:53:52 -0400

    remove virtual env from right prompt to avoid duplication

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -48,7 +48,7 @@
       command_execution_time  # duration of the last command
       background_jobs         # presence of background jobs
       direnv                  # direnv status (https://direnv.net/)
-      virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+      # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
       anaconda                # conda environment (https://conda.io/)
       pyenv                   # python environment (https://github.com/pyenv/pyenv)
       nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
